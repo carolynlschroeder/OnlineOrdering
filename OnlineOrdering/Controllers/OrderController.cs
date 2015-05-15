13ad8bf4c6cb37b7a06c4b2713f3d135d82a0db9
Carolyn Schroeder <carolynlschroeder@hotmail.com>
2015-05-15 15:17:46 -0500

    Started Order Controller

---
+++ OnlineOrdering/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineOrdering.Model;
+using OnlineOrderingBusiness.Repositories;
+
+namespace OnlineOrdering.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult CreateOrder()
+        {
+            var repository = new CustomerRepository();
+            var customerDictionary =
+                repository.GetCustomers()
+                    .Select(c => new {CustomerId = c.CustomerId, CustomerName = c.FirstName + " " + c.LastName})
+                    .ToDictionary(x => x.CustomerId, x => x.CustomerName);
+            var model = new OrderModel
+            {
+                OrderId = Guid.NewGuid(),
+                OrderDateTime = DateTime.Now,
+                CustomerDictionary = customerDictionary
+            };
+            return View(model);
+        }
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+    }
+}
